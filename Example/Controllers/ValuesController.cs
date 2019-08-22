3aeca51e3e5cc2e1b55dc566e23239198ccf48e0
Niz <FernandoGGNiz@gmail.com>
2019-08-22 12:14:51 -0300

    Conexion DB

---
+++ Example/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Example.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(BloggingContext bloggingContext)
+        {      
+            Context = bloggingContext;
+        }
+        private BloggingContext Context;
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Blog>> Get()
+        {
+            return Context.Blogs.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
